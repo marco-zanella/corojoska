4904f22afb10d43f1871525d9a4a119c456202f2
Marco Zanella <mz@openmailbox.org>
2017-06-06 15:37:59 +0200

    Added basic view parts

---
+++ src/View/head.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title><?php echo $title; ?> | Coro Monte Pasubio</title>
+<meta name="description" content="<?php echo $description; ?>">
+<meta name="author" content="Coro della Joska">
+<meta name="keywords" content="coro,joska,rovigo,liceo,<?php echo $title; ?>">
+<link rel="canonical" href="">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<?php $this->view('opengraph', $_variables); ?>
