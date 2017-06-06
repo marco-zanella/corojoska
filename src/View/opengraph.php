19a1b772b1144fd3f5a94f617c14340abf3975b2
Marco Zanella <mz@openmailbox.org>
2017-06-06 15:31:52 +0200

    Added Facebook Open Graph view

---
+++ src/View/opengraph.php
@@ -0,0 +1,8 @@
+<!-- Facebook Open Graph information -->
+<meta property="fb:app_id" content="">
+<meta property="og:title" content="<?php echo $title; ?>">
+<meta property="og:type" content="website">
+<meta property="og:image" content="">
+<meta property="og:url" content="">
+<meta property="og:description" content="<?php echo $description; ?>">
+<meta property="og:site_name" content="Coro della Joska">
