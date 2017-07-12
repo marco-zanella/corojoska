a21f730937a333c0d55d4de00cabc1b3ec593a7c
Marco Zanella <mz@openmailbox.org>
2017-07-13 00:04:14 +0200

    Added permission model

---
+++ src/Model/Permission.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of Corojoska.
+ *
+ * Corojoska is distributed under the hope it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Corojoska. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ *
+ * @author    Marco Zanella <mz@openmailbox.org>
+ * @copyright 2017 Coro della Joska
+ * @license   GNU General Public License, version 3
+ */
+namespace Joska\Model;
+
+/**
+ * A permission of a user.
+ * 
+ * This class follows the Model-View-Controller Pattern.
+ * 
+ * @author Marco Zanella <mz@openmailbox.org>
+ * @copyright 2017 Coro della Joska
+ * @package Joska\Model
+ */
+class Permission extends Model {
+}
