681ab24d659f1821fe245d5ef9401558767dcac5
Marco Zanella <mz@openmailbox.org>
2017-07-12 17:34:11 +0200

    Changed workstation

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
 <?php
-// Debugging purposes
-$_SERVER = [];
-$_SERVER['REQUEST_URI'] = '/image/' . urlencode('http://www.cssauthor.com/wp-content/uploads/2014/09/Free-Mockup-PSD1.jpg') . '/640/480/png';
-$_SERVER['REQUEST_METHOD'] = 'GET';
-
-$_POST = [];
-$_POST['content'] = '<h3>HTML content</h3>';
-// End
-
 error_reporting(E_ALL);
+
 require_once 'src/error_catcher.php';
 require_once 'src/autoloader.php';
 
-
-
-trigger_error('error msg');
-
-
-
-
-
 $router = new \Joska\Router();
-
 
 $router
     ->declareRoute('/test', 'Joska\Controller\Test')
