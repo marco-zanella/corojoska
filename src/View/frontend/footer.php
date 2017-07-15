164f1473b52f8c82a89769cdb34630aa11f7c87d
Marco Zanella <mz@openmailbox.org>
2017-07-15 15:35:38 +0200

    Updated footer information

--- src/View/frontend/footer.php
+++ src/View/frontend/footer.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="row">
     <div class="col-sm-7">
-      <p>Via Alcide De Gasperi, 19 - 45100 Rovigo - © 2017 coromontepasubio.it - Tutti i diritti riservati</p>
+      <p>Via Alcide De Gasperi, 19 - 45100 Rovigo - © 2017 corojoska.altervista.org - Tutti i diritti riservati</p>
     </div>
 
     <div class="col-sm-5">
