aec2a185fdc7d413856bbbc6bd2bb938aa465aa3
Marco Zanella <mz@openmailbox.org>
2017-07-16 16:52:47 +0200

    Added favicon

--- src/View/frontend/header.php
+++ src/View/frontend/header.php
@@ -1,6 +1,8 @@
 <div class="container">
   <div class="page-header header-container">
     <h1 class="header-title">Coro della Joska</h1>
-    <img src="http://www.mrwallpaper.com/wallpapers/deep-space.jpg" alt="logo" class="header-logo hidden-xs">
+    <div class="header-logo hidden-xs">
+      <a href="/home"><img src="/public/style/logo.svg" alt="logo" class="img-thumbnail"></a>
+    </div>
   </div>
 </div>
