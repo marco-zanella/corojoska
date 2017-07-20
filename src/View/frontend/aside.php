d169cc99ad3ecb06ad6cb8dddacf4b17b5b72152
Marco Zanella <mz@openmailbox.org>
2017-07-20 19:32:29 +0200

    Updated layout

--- src/View/frontend/aside.php
+++ src/View/frontend/aside.php
@@ -1,2 +1,5 @@
+<div class="hidden-sm hidden-xs">
+  <a href="/"><img src="/public/style/logo.svg" alt="logo" class="img-responsive img-thumbnail center-block"></a>
+</div>
 <?php $this->view('widgets/upcoming-events', ['upcoming_events' => $upcoming_events]); ?>
 <?php $this->view('widgets/latest-posts', ['latest_posts' => $latest_posts]); ?>
--- src/View/frontend/header.php
+++ src/View/frontend/header.php
@@ -1,11 +1,13 @@
 <div class="container">
   <div class="page-header header-container">
     <?php if ($show_header_image === true): ?>
-    <img src="/public/style/header-background.svg" class="img-responsive">
+    <img src="/public/style/header-background.svg" class="img-responsive hidden-xs">
     <?php endif; ?>
     <h1 class="header-title">Coro della Joska</h1>
+<!--
     <div class="header-logo hidden-xs">
       <a href="/"><img src="/public/style/logo.svg" alt="logo" class="img-thumbnail"></a>
     </div>
+-->
   </div>
 </div>
