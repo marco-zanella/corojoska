1745393a5ca263be1fdb5e5d3b3e2072253d86ed
Marco Zanella <mz@openmailbox.org>
2017-07-21 16:44:46 +0200

    Removed Google Font

--- src/View/frontend/head.php
+++ src/View/frontend/head.php
@@ -1,3 +1,2 @@
 <link rel="stylesheet" href="/public/style/frontend.css">
 <link rel="stylesheet" href="/public/style/teaser.css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
