aef728ad9d7b908104a69dee3858004b17c56d27
Marco Zanella <mz@openmailbox.org>
2017-08-26 13:38:23 +0200

    Fixed icon position in navbar

--- src/View/frontend/navbar.php
+++ src/View/frontend/navbar.php
@@ -20,27 +20,27 @@ $active = function ($name) use($section) {
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li<?= $active('home') ?>>
-          <a href="/">Homepage <span class="glyphicon glyphicon-home hidden-sm"></span></a>
+          <a href="/"><span class="glyphicon glyphicon-home hidden-sm"></span> Homepage</a>
         </li>
         <li<?= $active('calendario') ?>>
-          <a href="/calendario">Calendario <span class="glyphicon glyphicon-calendar hidden-sm"></span></a>
+          <a href="/calendario"><span class="glyphicon glyphicon-calendar hidden-sm"></span> Calendario</a>
         </li>
         <li<?= $active('biografia') ?>>
-          <a href="/biografia">Biografia <span class="glyphicon glyphicon-info-sign hidden-sm"></span></a>
+          <a href="/biografia"><span class="glyphicon glyphicon-info-sign hidden-sm"></span> Biografia</a>
         </li>
         <li<?= $active('contatti') ?>>
-          <a href="/contatti">Contatti <span class="glyphicon glyphicon-envelope hidden-sm"></span></a>
+          <a href="/contatti"><span class="glyphicon glyphicon-envelope hidden-sm"></span> Contatti</a>
         </li>
       </ul>
 
       <ul class="nav navbar-nav navbar-right">
         <?php if (\Joska\Session::isAuthenticated()): ?>
         <li>
-          <a href="/account">Area Riservata <span class="glyphicon glyphicon-user hidden-sm"></span></a>
+          <a href="/account"><span class="glyphicon glyphicon-user hidden-sm"></span> Area Riservata</a>
         </li>
         <?php else: ?>
         <li<?= $active('accedi') ?>>
-          <a href="/login">Accedi <span class="glyphicon glyphicon-log-in hidden-sm"></span></a>
+          <a href="/login"><span class="glyphicon glyphicon-log-in hidden-sm"></span> Accedi</a>
         </li>
         <?php endif; ?>
       </ul>
